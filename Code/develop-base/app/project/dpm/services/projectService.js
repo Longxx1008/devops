49108af073047cdd6f210239bad02bd913ea9d9d
xiangyong <xiangyong@hvgroup.com.cn>
2017-05-08 09:24:37 +0800

    修改首页 Signed-off-by: xiangyong <xiangyong@hvgroup.com.cn>

--- Code/develop-base/app/project/dpm/services/projectService.js
+++ Code/develop-base/app/project/dpm/services/projectService.js
@@ -1,5 +1,7 @@
 var utils = require('../../../common/core/utils/app_utils');
 var mysqlPool = require('../../utils/mysql_pool');
+var config = require('../../../../config');
+var https = require('https');
 
 /**
  * 获取项目的分页数据
@@ -35,11 +37,108 @@ exports.versionList = function(conditionMap, cb) {
         }
     }
     sql += " order by t.id desc";
+    //判断gitlab项目ID是否为空
+    if(conditionMap.gitProjectId){
+        //获取pipelines的Tags--status为success的版本数据
+        var options = {
+            hostname: config.platform.gitlabIp,
+            path: '/api/v3/projects/'+conditionMap.gitProjectId+'/pipelines?private_token='+config.platform.private_token+'&scope=tags',
+            rejectUnauthorized: false  // 忽略安全警告
+        };
+        var req = https.get(options, function (res) {
+            console.log(">>>>>>>>>>>>statusCode：" + res.statusCode + "<<<<<<<<<<<<<<");
+            res.setEncoding('utf8');
+            var chtmlJson = '';
+            res.on('data', function (chunk) {//拼接响应数据
+                chtmlJson += chunk;
+            });
+            res.on('end', function () {
+                var info = JSON.parse(chtmlJson);//将拼接好的响应数据转换为json对象
+                var obj;
+                var results = [];
+                if (info) {
+                    console.log("===info===="+info);
+                    for(var i=0;i<info.length;i++){
+                        if(info[i].status == 'success'){
+                            obj = {'vNo':info[i].ref,'projectId':conditionMap.projectId};
+                            results.push(obj);
+                        }
+                    }
+                    cb(utils.returnMsg(true, res.statusCode, '获取版本信息成功。', results, null));
+                } else {
+                    cb(utils.returnMsg(false, res.statusCode, '获取版本信息失败。', results, null));
+                }
+
+            });
+        });
+        req.on('error', function (err) {
+            console.error(err.code);
+            cb(utils.returnMsg(false, '404', err.message, null, null));
+        });
+
+    }else{
+        // 查询数据库默认版本数据
+        mysqlPool.query(sql,condition,function(err,results) {
+            if(err) {
+                cb(utils.returnMsg(false, '1000', '获取版本信息异常', null, err));
+            } else {
+                cb(utils.returnMsg(true, '0000', '获取版本信息成功', results, null));
+            }
+        });
+    }
+   
+}
+
+//根据projectCode获取项目数据
+exports.getProject = function(conditionMap, cb) {
+    var sql = " select t.* from pass_develop_project_resources t where 1=1 ";
+    var condition = [];
+    if(conditionMap) {
+        if(conditionMap.projectCode) {
+            sql += " and t.projectCode=?";
+            condition.push(conditionMap.projectCode);
+        }
+    }
     mysqlPool.query(sql,condition,function(err,results) {
         if(err) {
-            cb(utils.returnMsg(false, '1000', '获取版本信息异常', null, err));
+            cb(utils.returnMsg(false, '1000', '获取项目信息异常', null, err));
         } else {
-            cb(utils.returnMsg(true, '0000', '获取版本信息成功', results, null));
+            if(results&& results.length>0){
+                cb(utils.returnMsg(false, '1000', '项目已存在', results, null));
+            }else{
+                //获取gitlab所有项目
+                var options = {
+                    hostname: config.platform.gitlabIp,
+                    path: '/api/v3/projects?private_token='+config.platform.private_token,
+                    rejectUnauthorized: false  // 忽略安全警告
+                };
+                var req = https.get(options, function (res) {
+                    console.log(">>>>>>>>>>>>statusCode：" + res.statusCode + "<<<<<<<<<<<<<<");
+                    res.setEncoding('utf8');
+                    var chtmlJson = '';
+                    res.on('data', function (chunk) {//拼接响应数据
+                        chtmlJson += chunk;
+                    });
+                    res.on('end', function () {
+                        var info = JSON.parse(chtmlJson);//将拼接好的响应数据转换为json对象
+                        var obj;
+                        var results = [];
+                        if (info) {
+                            for(var i=0;i<info.length;i++){
+                                results.push({"projectId":info[i].id,"projectName":info[i].name});
+                            }
+                            cb(utils.returnMsg(true, res.statusCode, '获取项目信息成功。', results, null));
+                        } else {
+                            cb(utils.returnMsg(false, res.statusCode, '获取项目信息失败。', results, null));
+                        }
+
+                    });
+                });
+                req.on('error', function (err) {
+                    console.error(err.code);
+                    cb(utils.returnMsg(false, '404', err.message, null, null));
+                });
+            }
         }
     });
 }
