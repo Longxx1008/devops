951e25b25811304dd4207c61c0e0b9eefc1b8201
luoyadong <122454219@qq.com>
2017-05-23 14:58:22 +0800

    修改应用部署bug
    Signed-off-by: luoyadong <122454219@qq.com>

--- Code/develop-base/app_init.js
+++ Code/develop-base/app_init.js
@@ -1,7 +1,13 @@
+var deployedInfoSyncJob = require('./app/common/job/services/deployedInfoSyncJob');
+var projectService = require('./app/project/dpm/services/projectService');
 /**
  * 系统启动时加载的内容
  * @param app
  */
 exports.$ = function(app) {
-        
+    //启动健康检查定时任务
+    setInterval(function(){
+        deployedInfoSyncJob.doJob();
+        projectService.refreshMarathonLbInfo();
+    },10000);
 }
