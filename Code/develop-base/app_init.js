162bb9dd196b7de250bd4fbb26b7b73255b343ba
wuhaitao <1172004952@qq.com>
2017-09-21 16:25:14 +0800

    task
    Signed-off-by: wuhaitao <1172004952@qq.com>

--- Code/develop-base/app_init.js
+++ Code/develop-base/app_init.js
@@ -13,6 +13,6 @@ exports.$ = function() {
         /*deployedInfoSyncJob.doJob();
         projectService.refreshMarathonLbInfo();*/
         colonyManageService.syncColonyInfo();
-        /*updateHostInfo.getSalve();*/
+        updateHostInfo.getSalve();
     },1000*60*10);
 }
