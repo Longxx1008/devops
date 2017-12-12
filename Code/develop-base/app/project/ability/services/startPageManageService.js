054a0b6e652dbe41166811420adf38528bfa3af3
szx <15761630446@163.com>
2017-12-12 12:55:21 +0800

    展示页管理

--- Code/develop-base/app/project/ability/services/startPageManageService.js
+++ Code/develop-base/app/project/ability/services/startPageManageService.js
@@ -3,17 +3,26 @@
  */
 var utils = require('../../../common/core/utils/app_utils');
 var mysqlPool = require('../../utils/mysql_pool');
+//加载mysql
+//var mysql=require('../mysqlConn');
+//加载mysql-queues 支持事务
+//var queues=require('mysql-queues');
+// 加载async 支持顺序执行
+//var async=require('async');
+
+
+
 
 exports.getStartPageManage=function (page,size,conditionMap,cb) {
     console.log('===service==');
-    var sql="select * from pass_develop_startPage_info";
+    var sql="select * from pass_develop_homepage_info";
     var orderBy = "";
     var conditions=conditionMap;
     utils.pagingQuery4Eui_mysql(sql,orderBy, page, size, conditions, cb);
 }
 
 exports.add=function(data,cb){
-    var sql="insert into pass_develop_startPage_info(showType,imageName,imageType,imagePath) values(?,?,?,?) ";
+    var sql="insert into pass_develop_homepage_info(showType,imageName,imageType,imagePath) values(?,?,?,?) ";
     mysqlPool.query(sql,data,function(err){
         if(err) {
             cb(utils.returnMsg(false, '1000', '图片增加失败', null, err));
@@ -23,7 +32,7 @@ exports.add=function(data,cb){
     })
 };
 exports.getStartPageManageUpdate=function (page, size, data, cb) {
-    var sql='select * from pass_develop_startPage_info t where id="'+data+'"';
+    var sql='select * from pass_develop_homepage_info t where id="'+data+'"';
     var conditions=data;
     mysqlPool.query(sql,conditions,function(err,results) {
         if(err) {
@@ -45,14 +54,14 @@ exports.update=function (sql,data,cb) {
 };
 //删除
 exports.deleteInfo = function(id, cb) {
-    var sql="delete from pass_develop_startPage_info where id= "+id;
-
-    mysqlPool.query(sql,null,function (err) {
-        if(err) {
-            cb(utils.returnMsg(false, '1000', '删除失败', null, err));
-        } else {
-
-            cb(utils.returnMsg(true, '0000', '删除成功',null, null));
-        }
-    });
+    console.log("====deleteid",id);
+    //var ids=[ 14 , 15 ];
+        var sql='delete from pass_develop_homepage_info where id in ('+id+')';
+        mysqlPool.query(sql,null,function (err,result) {
+            if(err) {
+                cb(utils.returnMsg(false, '1000', '删除失败', null, err));
+            } else {
+                cb(utils.returnMsg(true, '0000', '删除成功',null, null));
+            }
+        });
 };
