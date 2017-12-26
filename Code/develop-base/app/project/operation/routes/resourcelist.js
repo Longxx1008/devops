5260ede7cdd335f58dfddfacf72a81c4f4dd16c2
wangwenjing <2312531314@qq.com>
2017-12-26 14:18:55 +0800

    修改资源列表页面及对应的后台逻辑

--- Code/develop-base/app/project/operation/routes/resourcelist.js
+++ Code/develop-base/app/project/operation/routes/resourcelist.js
@@ -6,18 +6,57 @@ var router = express.Router();
 var config = require('../../../../config');
 var resourceListService = require('../services/resourceListService');
 var utils = require('../../../common/core/utils/app_utils');
-router.route("/")
-    .get((req, res) => {
-        var page = req.query.page;
-        var length = req.query.rows;
-        resourceListService.pageList().then(rs=>{
-            utils.respJsonData(res,rs);
-        })
-    })
+/***********************资源列表查询(get),新增(put),修改(post)*******************************/
 
-router.route("/:id").get((req, res) => {
-    resourceListService.getResource(req.params.id).then(rs=>{
-        utils.respJsonData(res,rs);
-    })
-})
+router.route("/")
+    .get(function(req, res) {
+        var resource_number =req.query.resource_number;
+        var page = req.query.page;
+        var pageSize = req.query.rows;
+        var conditionMap = {};
+        if(resource_number){
+            conditionMap.resource_number = resource_number;
+        }
+        // 调用分页
+        resourceListService.pageList(page,pageSize,conditionMap,function(result){
+            utils.respJsonData(res, result);
+        });
+    }).put(function (req,res) {
+    var data = {};
+    data.resource_number = req.body.resource_number;
+    data.resource_name = req.body.resource_name;
+    data.resource_state = req.body.resource_state;
+    data.resource_apply = req.body.resource_apply;
+    data.resource_count = req.body.resource_count;
+    data.resource_remark = req.body.resource_remark;
+    resourceListService.add(data, function(result) {
+        utils.respJsonData(res, result);
+    });
+}).post(function (req,res) {
+    var data = [];
+    data.push(req.body.resource_number);
+    data.push(req.body.resource_name);
+    data.push(req.body.resource_state);
+    data.push(req.body.resource_apply);
+    data.push(req.body.resource_count);
+    data.push(req.body.resource_remark);
+    data.push(req.body.id);
+    console.log(req.body.resource_number+"1-++++++++++++++++++++++++++++++++++++++++++++++++++")
+    resourceListService.update(data, function(result) {
+        utils.respJsonData(res, result);
+    });
+});
+/************删除资源列表************/
+router.route('/:id')
+    .delete(function(req,res){
+        var id = req.params.id;
+        if(id) {
+            resourceListService.delete(id, function(result){
+                utils.respJsonData(res, result);
+            });
+        }
+        else {
+            utils.respMsg(res, false, '1000', 'ID不能为空。', null, null);
+        }
+    });
 module.exports = router;
--- Code/develop-base/app/project/operation/services/resourceListService.js
+++ Code/develop-base/app/project/operation/services/resourceListService.js
@@ -1,43 +1,89 @@
 /* 
-create by heidngxin 17.10.16
+create by wangwenjing 17.12.26
 */
 var utils = require('../../../common/core/utils/app_utils');
 var mysqlPool = require('../../utils/mysql_pool');
 var config = require('../../../../config');
 
 var https = require('https');
-exports.pageList = async function (page,size,conditionMap) {
-    /**
- * 查询资源列表
- * @param page
- * @param size
- * @param conditionMap
- * @returns Array<object>
- */
-return [{
-        id: 1,
-    resource_number: '12312398sdasd',
-        resource_name: 'mysql',
-        resource_state: 1,//  运行中 停止了
-        resource_apply: 'fttts',/* 实例名称 */
-        resource_count: 30,/* 资源占用情况 */
-        resource_remark: '我是备注',/* 备注 */
-        occupy_use:30,/* 占用值 */
-        occupy:100/* 总值 */
-    }]
-}
-/** 
-*@param id  
-*@returns object
-*/
-
-exports.getResource=async function (id){
-    return {  id: 1,
-        resource_number: '12312398sdasd',
-        resource_name: 'mysql',
-        resource_state: 1,//  运行中 停止了
-        resource_apply: 'fttts',/* 实例名称 */
-        resource_count: 30,/* 资源占用情况 */
-        resource_remark: '我是备注'/* 备注 */
+/**********************资源列表查询
+ * @param page 第几页
+ * @param size 每页多少条
+ * @param conditionMap 查询条件
+ *********************************/
+exports.pageList = async function (page,size,conditionMap,cb) {
+    var sql = "select * from pass_develop_project_resourcelist where 1=1";
+    var conditions = [];
+    if(conditionMap){
+        if(conditionMap.resource_number){
+            sql += " and (resource_number like '%" + conditionMap.resource_number + "%')";
+        }
     }
+    var orderBy = " order by id asc";
+    utils.pagingQuery4Eui_mysql(sql,orderBy,page, size, conditions, cb);
+}
+
+/**
+ * 新增资源列表信息
+ * @param data
+ * @param cb
+ */
+exports.add=async function (data,cb){
+    var dataList= [];
+    dataList.push(data.resource_number);
+    dataList.push(data.resource_name);
+    dataList.push(data.resource_state);
+    dataList.push(data.resource_apply);
+    dataList.push(data.resource_count);
+    dataList.push(data.resource_remark);
+    var sql = "insert into pass_develop_project_resourcelist(resource_number,resource_name,resource_state,resource_apply,resource_count,resource_remark) values('"+data.resource_number+"','"+data.resource_name+"','"+data.resource_state+"','"+data.resource_apply+"','"+data.resource_count+"','"+data.resource_remark+"')";
+    mysqlPool.query(sql,dataList,function(err,result) {
+        if(err) {
+            cb(utils.returnMsg(false, '1000', '新增失败', null, err));
+        } else {
+            cb(utils.returnMsg(true, '0000', '新增成功', result, null));
+        }
+    });
+}
+
+
+/**
+ * 修改资源列表信息
+ * @param data
+ * @param cb
+ */
+exports.update = async function(data,cb){
+    var resource_number = data[0];
+    var resource_name = data[1];
+    var resource_state = data[2];
+    var resource_apply = data[3];
+    var resource_count = data[4];
+    var resource_remark = data[5];
+    var id = data[6];
+    console.log(data[0],data[1],data[2],data[3],data[4],'++++++++++++++++++++++++++++++++',data[5]+'+++++++++++++++++++++++++++++++++++++++++++++++99999999999999999999999999999999999999999999')
+    var sql = "update pass_develop_project_resourcelist set resource_number='"+resource_number+"',resource_name='"+resource_name+"',resource_state='"+resource_state+"',resource_apply='"+resource_apply+"',resource_count='"+resource_count+"',resource_remark='"+resource_remark+"' where id='"+id+"'";
+    mysqlPool.query(sql,data,function(err,result) {
+        if(err) {
+            cb(utils.returnMsg(false, '1000', '更新策略异常', null, err));
+        } else {
+            cb(utils.returnMsg(true, '0000', '更新策略成功', result, null));
+        }
+    });
+};
+
+/**
+ * 删除资源列表信息
+ * @param id
+ * @param cb
+ */
+exports.delete = async function(id, cb) {
+    var sql = "delete from pass_develop_project_resourcelist where id = ?";
+    mysqlPool.query(sql,[id],function (err,result) {
+        if(err){
+            cb(utils.returnMsg(false,'1000','删除资源列表信息异常',null,err));
+        }else{
+            cb(utils.returnMsg(true,'0000','删除资源列表信息成功',result,null));
+        }
+    })
+
 }
