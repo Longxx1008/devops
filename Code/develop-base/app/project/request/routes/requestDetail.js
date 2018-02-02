536a00e659fa557a7ef37db3b753252bc5c0edfa
ylming <1079848166@qq.com>
2018-02-02 10:46:12 +0800

    request

--- Code/develop-base/app/project/request/routes/requestDetail.js
+++ Code/develop-base/app/project/request/routes/requestDetail.js
@@ -18,19 +18,20 @@ router.route('/getDetailByNumber').get(function(req,res){
     var conditionMap = {};
     conditionMap.serial_number=req.query.serial_number;
 
-    requestDetailService.getServiceType(0, 10, conditionMap).then(function(result){
+    requestDetailService.getServiceType(0, 10, conditionMap,function(result){
         var list={};
         // item.log("result------",result);
+        var j=0;
         for (var i=0;i<result.total;i++){
             var item="'"+result.rows[i].micro_service_id+"'";
-            console.log("result11-----",item);
 
-            requestDetailService.getDetailMsgByNumber(result.rows[i].micro_service_id).then(function(result1){
-                // console.log("result11-----",result1);
-                console.log("result2-----",item);
-                list[item]=result1.rows;
-                // list[i]=result1;
 
+            requestDetailService.getDetailMsgByNumber(result.rows[i].micro_service_id,function(result1){
+
+                if(list[j]=result1){
+                    console.log("result333-----",list[j].rows[0].module);
+                    j=j+1;
+                }
             });
         }
 
@@ -40,6 +41,35 @@ router.route('/getDetailByNumber').get(function(req,res){
 });
 
 
+
+//
+//
+// //文章详情Controller
+// router.route('/getDetailByNumber').get(function(req,res){
+//     var conditionMap = {};
+//     conditionMap.serial_number=req.query.serial_number;
+//
+//     requestDetailService.getServiceType(0, 10, conditionMap).then(function(result){
+//         var list={};
+//         // item.log("result------",result);
+//         for (var i=0;i<result.total;i++){
+//             var item="'"+result.rows[i].micro_service_id+"'";
+//             console.log("result11-----",item);
+//
+//             requestDetailService.getDetailMsgByNumber(result.rows[i].micro_service_id).then(function(result1){
+//                 // console.log("result11-----",result1);
+//                 console.log("result2-----",item);
+//                 list[item]=result1.rows;
+//                 // list[i]=result1;
+//
+//             });
+//         }
+//
+//         utils.respJsonData(res, list);
+//     });
+//
+// });
+//
 
 
 
--- Code/develop-base/app/project/request/services/requestDetailService.js
+++ Code/develop-base/app/project/request/services/requestDetailService.js
@@ -8,22 +8,45 @@ var utils = require('../../../common/core/utils/app_utils');
 
 
 exports.getDetailMsgByNumber = function (service_id, cb) {
-    var p = new Promise(function(resolve,reject) {
+
     var sql = "SELECT * from pass_project_method_info where  req_serial_num=201801301130001 and micro_service_id="+service_id;
     var orderBy = '';
-        resolve(utils.pagingQuery4Eui_mysql(sql, orderBy, 0, 10, null, cb));
-    });
-    // return p;
+    utils.pagingQuery4Eui_mysql(sql, orderBy, 0, 10, null, cb);
+
     };
 
 
 exports.getServiceType = function (page, size, conditionMap, cb) {
-    var p = new Promise(function(resolve,reject) {
+
         var sql = "SELECT distinct micro_service_id from pass_project_method_info where  req_serial_num=201801301130001";
         var conditions = [];
         var orderBy = '';
-        resolve(utils.pagingQuery4Eui_mysql(sql, orderBy, page, size, conditions, cb));
+        utils.pagingQuery4Eui_mysql(sql, orderBy, page, size, conditions, cb);
 
-    });
+
     // return p;
 };
+
+
+
+//
+// exports.getDetailMsgByNumber = function (service_id, cb) {
+//     var p = new Promise(function(resolve,reject) {
+//     var sql = "SELECT * from pass_project_method_info where  req_serial_num=201801301130001 and micro_service_id="+service_id;
+//     var orderBy = '';
+//         resolve(utils.pagingQuery4Eui_mysql(sql, orderBy, 0, 10, null, cb));
+//     });
+//     // return p;
+//     };
+//
+//
+// exports.getServiceType = function (page, size, conditionMap, cb) {
+//     var p = new Promise(function(resolve,reject) {
+//         var sql = "SELECT distinct micro_service_id from pass_project_method_info where  req_serial_num=201801301130001";
+//         var conditions = [];
+//         var orderBy = '';
+//         resolve(utils.pagingQuery4Eui_mysql(sql, orderBy, page, size, conditions, cb));
+//
+//     });
+//     // return p;
+// };
