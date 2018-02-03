6fc18d31cda398740d3016444dc3d293b013d91b
何定鑫 <514803310@qq.com>
2018-02-03 10:14:48 +0800

    新增db.queryPromise  方法查询数据库 api端口模块构建

---
+++ Code/develop-base/app/project/operation/services/apigatawayService.js
@@ -0,0 +1,9 @@
+'use strict';
+/**
+ * Created by hedingxin on 2018/2/2
+ */
+module.export = {
+  async getAllAppWithApi(){},//获取应用
+  async createNewApi(){},//创建新的api
+  async trafficBreaking(){},//流量阻断
+};
